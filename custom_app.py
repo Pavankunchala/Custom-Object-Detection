53b34f38a9258e4f67870221c3f99bc7c1c26196
Pavan Kunchala <pavankunchalapk@gmail.com>
2021-07-18 14:07:45 +0530

    Add files via upload

--- custom_app.py
+++ custom_app.py
@@ -40,6 +40,37 @@ flags.DEFINE_boolean('plate', False, 'perform license plate recognition')
 
 DEMO_VIDEO = 'new.mp4'
 
+def image_resize(image, width=None, height=None, inter=cv2.INTER_AREA):
+    # initialize the dimensions of the image to be resized and
+    # grab the image size
+    dim = None
+    (h, w) = image.shape[:2]
+
+    # if both the width and height are None, then return the
+    # original image
+    if width is None and height is None:
+        return image
+
+    # check to see if the width is None
+    if width is None:
+        # calculate the ratio of the height and construct the
+        # dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
+
+    # otherwise, the height is None
+    else:
+        # calculate the ratio of the width and construct the
+        # dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    # resize the image
+    resized = cv2.resize(image, dim, interpolation=inter)
+
+    # return the resized image
+    return resized
+
 def main(_argv):
 
     st.title('Custom Object Detection using Streamlit')
@@ -214,11 +245,14 @@ def main(_argv):
         result = np.asarray(image)
         #cv2.namedWindow("result", cv2.WINDOW_AUTOSIZE)
         result = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
         
         #if not FLAGS.dont_show:
             #cv2.imshow("result", result)
         if save_video:
             out.write(result)
+        
+        result = image_resize(result,width=720)
 
         stframe.image(result,channels = 'BGR',use_column_width=True)
         
